95711782129d96777e8e57e9fe4e0f3484eaefd1
ridu101 <ridu116540@gmail.com>
2025-09-30 12:24:14 +0600

    32-4 done

---
+++ fetch.js
@@ -0,0 +1,28 @@
+
+const loadData = () =>{
+
+   
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+     // promise of response
+    .then(response => response.json())
+    // promise of data
+    .then ( data => console.log(data))
+
+}
+
+const loadPost=()=>{
+    const url='https://jsonplaceholder.typicode.com/posts#'
+
+    fetch(url)
+    .then ( res=> res.json())
+    .then ( data => {
+        console.log(data)
+        displayPost(data)
+    })
+}
+
+const displayPost = (post) =>{
+  post.forEach( post =>{
+    console.log(post)
+  })
+}
---
+++ scipt.js
@@ -0,0 +1,33 @@
+// // console.log('JS File Connected');
+
+// const person ={
+//     name: 'Modhu',
+//     age: 22,
+//     profession: 'Student',
+//     money: 5000,
+//     friends:[   'Raju', 'Kalu', 'Saju', 'Babu'],
+//     isRich: false
+
+// }
+// // with out json file
+// console.log(person, typeof person);
+// console.log('------------------');
+// //  json file
+// const personJSON= JSON.stringify(person)
+// console.log(personJSON, typeof personJSON);
+
+// // json to object
+// console.log('------------------');
+// const parseObject = JSON.parse(personJSON);
+// console.log(parseObject, typeof parseObject);
+
+// // note to be remember
+// // 1. object to json => JSON.stringify(object)
+// // 2. json to object => JSON.parse(json)
+
+// // const result =fetch('https://jsonplaceholder.typicode.com/todos/1')
+// //       .then(response => response.json())
+// //       .then(json => console.log(json))
+
+// //       console.log(result);
+
