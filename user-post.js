fa536b694d5f091d95533a001fac931a286db972
ridu101 <ridu116540@gmail.com>
2025-09-30 15:11:29 +0600

    32-7 done

--- user-post.js
+++ user-post.js
@@ -1,51 +1,45 @@
 document.getElementById('post-btn')
-    .addEventListener('click', function () {
+  .addEventListener('click', function () {
+    const url = 'https://jsonplaceholder.typicode.com/users';
 
-        const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        displayPost(data);
+      });
+  });
 
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                displayPost(data)
-            })
-
-    })
-
-//   now make the function of loading and displaying the posts
-
+// Function to display posts (users)
 const displayPost = posts => {
-    // 1. get the post container 
-    const postContainer = document.getElementById('post-container');
-    postContainer.innerHTML = '';
-    
+  const postContainer = document.getElementById('post-container');
+  postContainer.innerHTML = '';
 
-    // 2. loop through the posts
+  posts.forEach(post => {
+    const postDiv = document.createElement('div');
+    postDiv.className = 'post-card';
 
-    posts.forEach(post => {
+    postDiv.innerHTML = `
+      <p><strong>Id:</strong> ${post.id}</p>
+      <p><strong>Username:</strong> ${post.username}</p>
+      <p><strong>Email:</strong> ${post.email}</p>
 
-        // 3. create a div for each post
-        const postDiv = document.createElement('div');
-        postDiv.innerHTML = `<div class="post-card">
-            <p>Id: ${post.id}</p>
-            <p>Name:${post.username}</p>
-            <p>Email: ${post.email}</p>
-            <!-- address div -->
-             <div>
-                  ${post.address}}
-             </div>
-            <p>Phone: ${post.phone}</p>
-            <p>Website: ${post.website}</p>
-            <!-- company div  -->
-             <div>
-                  ${post.company}
-             </div>
-         </div>
-             
-            `
+      <!-- address -->
+      <div>
+        <strong>Address:</strong>
+        <p>${post.address.suite}, ${post.address.street}, ${post.address.city}, ${post.address.zipcode}</p>
+      </div>
 
-        // 4 append each post to the container
-        postContainer.append(postDiv)
-    });
+      <p><strong>Phone:</strong> ${post.phone}</p>
+      <p><strong>Website:</strong> <a href="http://${post.website}" target="_blank">${post.website}</a></p>
 
+      <!-- company -->
+      <div>
+        <strong>Company:</strong>
+        <p>${post.company.name}</p>
+        <p><em>${post.company.catchPhrase}</em></p>
+      </div>
+    `;
 
-}
+    postContainer.append(postDiv);
+  });
+};
